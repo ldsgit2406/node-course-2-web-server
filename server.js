ba4acffdc3040ee88c1d99dc70860962242c2881
ldsgit2406 <Dhanasekar@DESKTOP-RK2B4CQ>
2018-09-23 19:53:30 +0530

    Adding projects page

--- server.js
+++ server.js
@@ -43,6 +43,10 @@ app.get('/', (req, res) => {
   })
 });
 
+app.get('/projects',(req, res) => {
+  res.render('projects.hbs');
+});
+
 app.get('/about',(req, res) => {
   res.render('about.hbs',{
     pageTitle : 'About page title'
